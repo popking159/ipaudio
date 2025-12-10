86311b59b13e7f3a7416c9a273a5d8dc3eb7649d
popking159 <47087407+popking159@users.noreply.github.com>
2025-12-10 05:17:58 +0200

    Update version number to 8.0 in installer script

--- installer-ipaudio.sh
+++ installer-ipaudio.sh
@@ -9,7 +9,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Version info (update these for each release)
-version="8.1"
+version="8.0"
 description="Multi-category playlists, new skins, audio/video delay control"
 ipk_url="https://github.com/popking159/ipaudio/releases/download/IPAudio/enigma2-plugin-extensions-ipaudio_${version}_all.ipk"
 
